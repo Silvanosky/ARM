48fa9654531da0a469b4f7d08f9b8c99c327df99
Maxime Mehio <maxime.mehio@epita.fr>
2019-07-05 15:52:58 +0200

    corrected python script

--- Project/script/main.py
+++ Project/script/main.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python
 import serial
 
 port = "/dev/ttyUSB0"
 baud = 115200
 
 try:
-    com = serial.Serial(port, baud)
+    com = serial.Serial(port, baud, timeout=10)
     print("Serial communication: " + port + " " + str(baud))
     if com.isOpen():
         print("Serial opened")
@@ -12,15 +13,24 @@ try:
         print("Serial open failed")
         exit()
 
-    sha = open("sha")
-    sha_txt = sha.read()
+    #readong public key
+    publicKey = ""
+    while len(publicKey) < 4:
+        print("reading public key")
+        publicKey = com.readline()
+    print("finished reading, acquired rsa key = " + publicKey)
+
+    with  open("sha", 'r') as sha:
+        sha_txt = sha.read()
     print("SHA256 is: " + sha_txt)
     com.write(sha_txt.encode('ascii'))
     print("SHA written")
 
-    print("Receiving signature")
-    sign = com.read()
+    sign = ""
+    while len(sign) < 4 :
+        print("Receiving signature")
+        sign = com.readline()
     print("Signature: " + sign)
-except:
-    print("Exception thrown")
+except exc:
+    print("Exception thrown" + exc)
     exit()
