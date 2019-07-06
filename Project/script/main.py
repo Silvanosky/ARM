7122c942ce713c6864deae04ad8a5eabfe1045e7
Thomas Delecroix <thomas.delecroix@epita.fr>
2019-07-06 18:24:22 +0200

    Python: fix prints and delete debug

--- Project/script/main.py
+++ Project/script/main.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 import serial
+import base64
 
 port = "/dev/ttyUSB0"
 baud = 115200
+
+def password_configuration():
+    print("No password configured, please type one:")
 
 def debug(f):
     while True:
@@ -26,36 +30,32 @@ try:
         publicKey = ""
         while len(publicKey) < 38:
             publicKey = com.readline()
-            print("len pub = " + str(len(publicKey)))
+            print("Length key: " + str(len(publicKey)))
         print("Finished reading, RSA public key: " + publicKey.decode("ascii"))
 
         with open("sha", 'r') as sha:
             sha_txt = sha.readline()
         sha_txt = sha_txt + '\0'
-        print("SHA   : " + sha_txt)
+        print("SHA: " + sha_txt)
         transmitted = False
         res = ""
-        while not transmitted :
+        while not transmitted:
             com.write(sha_txt.encode('ascii'))
             res = com.readline()
             if res == b"errorBUSY\n":
-                print("busy")
+                print("BUSY")
             elif res ==  b'errorTIMEOUT\n':
                 print("TIMEOUT")
             elif res ==  b'errorERROR\n':
                 print("ERROR")
             else :
                 transmitted = True
-        print("res : " + str(res))
-        while True:
-            r = com.readline()
-            print("rest : " + str(r))
-        #print("SHA written")
+        print("Received: " + str(res))
 
-        #print("Receiving signature")
-        #debug(com)
-        #sign =  com.readline()
-        #print("Signature: " + str(sign))
+        print("SHA written")
+        print("Receiving signature")
+        sign =  com.readline()
+        print("Signature: " + str(sign))
 
 except Exception as e:
     print("Exception thrown: " + str(e))
